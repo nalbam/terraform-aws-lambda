dda5348ff5efb5ace9be58a7ac18cb0d989f6dff
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-05 20:16:32 +0900

    description

--- variables.tf
+++ variables.tf
@@ -6,48 +6,51 @@ variable region {
 
 variable name {
   default = "demo"
+  description = "Solution name, e.g. 'app' or 'demo'"
 }
 
 variable stage {
   default = "dev"
+  description = "Stage, e.g. 'prod', 'dev', 'stage', or 'test'"
 }
 
 variable description {
   default = ""
+  description = "Description of what your Lambda Function does."
 }
 
 variable "runtime" {
   //default = ""
-  description = ""
+  description = "See Runtimes for valid values."
 }
 
 variable "handler" {
   //default = ""
-  description = ""
+  description = "The function entrypoint in your code."
 }
 
 variable "memory_size" {
   default = "512"
-  description = ""
+  description = "Amount of memory in MB your Lambda Function can use at runtime."
 }
 
 variable "timeout" {
   default = "5"
-  description = ""
+  description = "The amount of time your Lambda Function has to run in seconds."
 }
 
 variable "bucket" {
   //default = ""
-  description = ""
+  description = "The S3 bucket location containing the function's deployment package."
 }
 
 variable "package" {
   //default = ""
-  description = ""
+  description = "The S3 key of an object containing the function's deployment package."
 }
 
 variable "variables" {
   type = "map"
   default = {}
-  description = ""
+  description = "A map that defines environment variables for the Lambda function."
 }
